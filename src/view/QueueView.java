948af016497a70d7bf651c65ec712534c24328c1
Stephan <stephanvisser12@gmail.com>
2019-01-31 16:07:25 +0100

    Added QueueView

---
+++ src/view/QueueView.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.*;
+
+import model.SimulatorModel;
+
+public  class QueueView extends AbstractView {
+
+    private JLabel test;
+
+    public QueueView(SimulatorModel model) {
+        super(model);
+        test = new JLabel("test");
+        add(test);
+        updateView();
+        setVisible(true);
+    }
+
+    private void setTest(int adHoc, int pass) {
+        test.setText("<html><h3>Auto's in wachtrij (Ad-Hoc): " + adHoc + ".<br/> Auto's in wachtrij (Abonnement): " + pass +"</h3></html>");
+    }
+
+    public void updateView() {
+     // SimulatorModel model = (SimulatorModel) super.model;
+
+        int adHocQueue = model.getCarQueueCount();
+        int passQueue = model.getPassQueueCount();
+        setTest(adHocQueue, passQueue);
+        repaint();
+    }
+}
