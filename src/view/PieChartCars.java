82780a29fef19e28d284229a974dcdd5a0f35498
KarenchoA <arkojan1@hotmail.com>
2019-02-01 11:35:37 +0100

    Modified

--- src/view/PieChartCars.java
+++ src/view/PieChartCars.java
@@ -11,21 +11,23 @@ public class PieChartCars extends AbstractView{
         setSize(200,200);
     }
 
+    	//paintcomponent is een methode van Java. setcolor maakt de kleur aan van het blokje, fillrect is de grootte van het blokje.
     public void paintComponent(Graphics g) {
     	g.setColor(Color.WHITE);
         g.fillRect(0, 0, 200, 200);
 
+        //hier definiteren we de hoevheelheid, dat word uit simulator model gehaald.
         int amountOfAd_Hoc = model.getAmountOfAd_Hoc();
         int amountOfPassCars = model.getAmountOfPassCars();
         int amountOfReservedCars = model.getAmountOfReservedCars();
 
-        //360 degree circle / 540 parkeerplekken
+        //360 degree circle gedeeld door 540 parkeerplekken met de bijbehorende formules
         float equalizer = 360f / 540f;
         int angleAd_Hoc = Math.round(amountOfAd_Hoc * equalizer);
         int anglePassCars = Math.round(amountOfPassCars * equalizer);
         int angleReservedCars = Math.round(amountOfReservedCars);
         
-        //Ad_Hoc stukje
+        //Ad_Hoc stukjes met het begin van de angle en het einde
         g.setColor(Color.RED);
         g.fillArc(10, 10, 180, 180, 0, angleAd_Hoc);
         //PassCars stukje
